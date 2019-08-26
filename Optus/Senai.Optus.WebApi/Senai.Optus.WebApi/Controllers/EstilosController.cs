b65ba3c14fe95783a12d31638bd6209a034e1593
Rayssa Tavares Dos Santos <46445396860@info.local>
2019-08-26 17:25:26 -0300

    up

---
+++ Optus/Senai.Optus.WebApi/Senai.Optus.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+
+    public class EstilosController : ControllerBase
+    {
+        EstiloRepository EstiloRepository = new EstiloRepository();
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(EstiloRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar (Estilos estilo)
+        {
+            try
+            {
+                EstiloRepository.Cadastrar(estilo);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Cago no pau, veado" + ex.Message });
+            }
+        }
+    }
+}
---
+++ Optus/Senai.Optus.WebApi/Senai.Optus.WebApi/Repositorys/EstiloRepository.cs
@@ -0,0 +1,28 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositorys
+{
+    public class EstiloRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.ToList();
+            }
+        }
+
+        public void Cadastrar(Estilos estilo)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                ctx.Estilos.Add(estilo);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
